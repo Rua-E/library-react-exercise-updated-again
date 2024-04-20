07573e6c1a635a4535109afd3446fec76eacec48
Rua <rua.elkasheef@gmail.com>
2024-04-19 22:39:23 -0400

    added loading state to books

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
 import Rating from "./Rating";
 import Price from "./Price";
 
 const Book = ({ book }) => {
+  const [img, setImg] = useState();
+
+
+  useEffect(() => {
+    const image = new Image()
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout(() => {
+          setImg(image);
+      }, 300)
+    };
+   
+  })
+
   return (
     <>
       <div className="book">
-        <Link to={`/books/${book.id}`}>
+        {img ? 
+          (<>
+            <Link to={`/books/${book.id}`}>
           <figure className=".book__img--wrapper">
             <img
-              src={book.url}
+              src={img.src}
               alt=""
               className="book__img"
             />
@@ -23,6 +39,13 @@ const Book = ({ book }) => {
         </div>
         <Rating rating={book.rating} />
         <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
+          </>) : ( <>
+            <div className="book__img--skeleton"></div>
+            <div className="skeleton book__title--skeleton"></div>
+            <div className="skeleton book__rating--skeleton"></div>
+            <div className="skeleton book__price--skeleton"></div>
+          </>)
+        }
       </div>
     </>
   );
